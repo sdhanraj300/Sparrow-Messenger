1ac4354c0817b11ad1fbffd7b20c5adca1053e94
ashucoder1 <mailtoashuyadav@gmail.com>
2023-02-12 17:23:15 +0530

    firebase integration and lottie animation implementation and use . gradient home screen

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+   val db= Firebase.firestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val user = hashMapOf(
+            "first" to "3",
+            "last" to "Voluter"
+        )
+
 
 
 
